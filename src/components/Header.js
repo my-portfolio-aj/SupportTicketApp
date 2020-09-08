e4124e813a4b78278a6432f19f0c84bdabf1587f
Antoinette <create@aplusmedia.info>
2020-09-07 23:04:42 -0400

    more changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react"
 import logo from "../logo.png"
-
+import {Link} from "react-router-dom"
 
 function Header(){
     return(
         <div className = "App-header">
             <h1 className = "header-text">In</h1> <img className = "App-logo" src={logo} alt="Logo" />
+            <nav className="nav">
+                <Link className="nav" to="/registration">Registration</Link>
+            </nav>
         </div>
     )
 }
